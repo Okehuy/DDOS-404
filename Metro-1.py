5db9f38f68dee6a2c6cf98b514d8e1f9c3feb3b9
Okehuy <141473753+Okehuy@users.noreply.github.com>
2023-08-12 13:01:48 +0700

    Update Metro-1.py

--- Metro-1.py
+++ Metro-1.py
@@ -14,7 +14,7 @@ os.system("clear")
 
 ip = sys.argv[1]
 port = sys.argv[2]
-orgip =ip
+orgip = ip
 
 Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
                        codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
